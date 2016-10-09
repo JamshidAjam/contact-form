dc6896c594a3064415a2e0fda63f7c7fa4d048f4
Jamshid <mr.jamshid.a@gmail.com>
2016-10-09 16:33:16 +0330

    completed the form

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+
+<?php
+        
+    $error = "";
+    $successMessage = "";
+
+    if($_POST) {
+        if(!$_POST["email"]) {
+            $error .= "you need to enter your email.<br>";
+        }
+        if(!$_POST["subject"]) {
+            $error .= "you need to enter your subject.<br>";
+        }
+        if(!$_POST["message"]) {
+            $error .= "you need to enter your message.<br>";
+        }
+        if($_POST["email"] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
+            $error .= "the email address is invalid.<br>";
+        }
+        
+        if($error != "") {
+            $error = '<div class="alert alert-danger" role="alert"><strong>There were error(s)</strong><br>' . $error . '</div>';
+        } else {
+            $emailTo = "mr.jamshid.a@gmail.com";
+            $subject = $_POST["subject"];
+            $message = $_POST["message"];
+            $headers = "From: " . $_POST["email"];
+            
+            if(mail($emailTo, $subject, $message, $headers)) {
+                $successMessage = '<div class="alert alert-success" role="alert">Your message sent successfully.<br>';
+            } else {
+                $error = '<div class="alert alert-danger" role="alert"><strong>Your message couldn\'t be sent. Please try again later.</strong></div>';
+            }
+        }
+    }
+
+?>
+
 <html>
 <head>
     <title>Contact Form</title>
@@ -12,22 +51,22 @@
     
     <h1>Get in touch!</h1>
     
-    <div id="error"></div>
+    <div id="error"><?php echo $error.$successMessage ?></div>
     
-    <form>
+    <form method="post">
         <div class="form-group">
             <label for="Email1">Email address</label>
-            <input type="email" class="form-control" id="email1" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
             <label for="subject">Subject</label>
-            <input type="text" class="form-control" id="subject">
+            <input type="text" class="form-control" id="subject" name="subject">
         </div>
 
         <div class="form-group">
-            <label for="content">What would you like to ask us?</label>
-            <textarea class="form-control" id="message" rows="3"></textarea>
+            <label for="message">What would you like to ask us?</label>
+            <textarea class="form-control" id="message" name="message" rows="3"></textarea>
         </div>
         <button type="submit" id="submit" class="btn btn-primary">Submit</button>
     </form>
@@ -41,9 +80,12 @@
     
     <script type="text/javascript">
         $("form").submit(function (e) {
-            e.preventDefault();
             
             var error = "";
+            
+            if($("#email").val() == "") {
+                error += 'the email field is empty!<br>';
+            }
             
             if($("#subject").val() == "") {
                 error += 'the subject field is empty!<br>';
@@ -54,9 +96,10 @@
             }
             
             if(error != "") {
-                $("#error").html('<div class="alert alert-danger" role="alert"><strong>There were error(s)</strong><br>' + error +'</div>');
+                $("#error").html('<div class="alert alert-danger" role="alert"><strong>There were error(s)</strong><br>' + error + '</div>');
+                return false;
             } else {
-                $("form").unbind("submit").submit();
+                return true;
             }
             
         });
